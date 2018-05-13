5503c4bc8d6bdfabff9304eed60ef82f0b132111
gostekk <gostekk94@gmail.com>
2018-05-14 00:22:25 +0200

    Uncommented if statement in store

--- client/src/store/configureStore.js
+++ client/src/store/configureStore.js
@@ -8,9 +8,9 @@ export default (initialState = {}) => {
     applyMiddleware(thunk),
   ];
 
-  // if (process.env.CLIENT && process.env.NODE_ENV === 'development') {
+  if (process.env.CLIENT && process.env.NODE_ENV === 'development') {
     enhancers.push(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-  // }
+  }
 
   const store = createStore(
     combineReducers({
